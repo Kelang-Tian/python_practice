2a8bfdbc26cb41788c1f852ea3990d348bae03e3
kelang <1141180644@qq.com>
2018-11-06 18:44:06 +0800

    pythonhomework

---
+++ cannonball/GPA.py
@@ -0,0 +1,55 @@
+class Student:
+    def __init__(self, name, hours, points):
+        self.name = name
+        self.hours = float(hours)
+        self.points = float(points)
+
+    def getName(self):
+        return self.name
+
+    def getHours(self):
+        return self.hours
+
+    def getQPoints(self):
+        return self.points
+
+    def gpa(self):
+        return self.points/self.hours
+
+
+def makeStudent(infoStr):
+    # infoStr is a tab-separated line: name hours qpoints
+    # returns a corresponding Student object
+    name, hours, qpoints = infoStr.split("\t")
+    return Student(name, hours, qpoints)
+
+
+def main():
+    # open the input file for reading
+    filename = input("Enter the name of file: ")
+    infile = open(filename, 'r')
+
+    best = makeStudent(infile.readline())
+
+    for line in infile:
+        s = makeStudent(line)
+        if s.gpa() > best.gpa():
+            best = s
+
+    infile.seek(0)
+
+    for line in infile:
+        s = makeStudent(line)
+        # if multiple students are tied for the best GPA
+        if s.gpa() == best.gpa():
+            # print information about the best student
+            print("The best students is: ", s.getName())
+            print("========hours======== ", s.getHours())
+            print("=========GPA========= ", s.gpa())
+            print()
+
+    infile.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ cannonball/cannonball.py
@@ -0,0 +1,53 @@
+from math import sin, cos, radians
+
+
+def getInputs():
+    angle = float(input("Enter the launch angle (in degrees): "))
+    vel = float(input("Enter the initial velocity (in meters/sec): "))
+    h0 = float(input("Enter the initial height (in meters): "))
+    time = float(input("Enter the time interval between position calculations: "))
+    return angle, vel, h0, time
+
+
+class Projectile:
+    def __init__(self, angle, velocity, height):
+        self.xpos = 0.0
+        self.ypos = height
+        theta = radians(angle)
+        self.xvel = velocity * cos(theta)
+        self.yvel = velocity * sin(theta)
+
+    def getX(self):
+        return self.xpos
+
+    def getY(self):
+        return self.ypos
+
+    def update(self, time):
+        self.xpos = self.xpos + time * self.xvel
+        yvel1 = self.yvel - time * 9.8
+        self.ypos = self.ypos + time * (self.yvel + yvel1)/2.0
+        self.yvel = yvel1
+
+    # calculate the maximum height achieved by the cannonball
+    def maxheight(self):
+        self.maxheight = self.ypos
+        if self.yvel > 0:
+            time = self.yvel / 9.8
+            self.maxheight += time * (self.yvel/2.0)
+        return self.maxheight
+
+
+def main():
+    angle, vel, h0, time = getInputs()
+    ball = Projectile(angle, vel, h0)
+    maxheight = ball.maxheight()
+    while ball.getY() > 0:
+        ball.update(time)
+    print("\nDistance: {0: 0.1f} meters.".format(ball.getX()))
+    print("\nMaximum height : {0: 0.1f} meters.".format(maxheight))
+
+
+if __name__ == '__main__':
+    main()
+
